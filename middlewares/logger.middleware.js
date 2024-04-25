f16a75318896d2e0682699a2f63cf75aac90d0a2
joewish <72031491+joewish@users.noreply.github.com>
2024-04-25 11:30:24 +0530

    some

---
+++ middlewares/logger.middleware.js
@@ -0,0 +1,29 @@
+import winston from "winston";
+import {customErrorHandler} from "./errorHandler.js";
+export const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'error.log',level: 'error'}),
+    new winston.transports.File({ filename: 'combined.log',level:'info' })
+  ]
+});
+
+export const loggerMiddleware = async ( err,req, res, next) => {
+  const timestamp = new Date().toString();
+    const reqUrl = req.originalUrl;
+    const reqBody = JSON.stringify(req.body)
+    if(err){
+      if(err instanceof customErrorHandler) {
+        logger.error(`timestamp: ${timestamp},request Url: ${reqUrl},error message:${err.message}`);
+        next();
+        }else{
+          logger.error(`timestamp: ${timestamp},request Url: ${reqUrl},error message:Oops! Something went wrong... Please try again later!`);
+          next();
+      }
+    }else{
+      logger.info(`timestamp: ${timestamp},request Url: ${reqUrl},message:${reqBody}`)
+    }
+  
+};
+export default loggerMiddleware;
